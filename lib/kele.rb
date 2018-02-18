a0c4fd9fd688016dc756a6cb82a3b5854bd4cf5e
David Alderson <dsa027@gmail.com>
2018-02-18 22:19:13 +0000

    rescue auth error

--- lib/kele.rb
+++ lib/kele.rb
@@ -4,9 +4,15 @@ class Kele
 include HTTParty
 
   def initialize(email, password)
-    @base_uri = "https://www.bloc.io/api/v1"
-    @auth_token = self.class.post(@base_uri + "/sessions", body: { "email": email, "password": password })
-    
-    p @auth_token
+    begin
+      @base_uri = "https://www.bloc.io/api/v1"
+      response = self.class.post(@base_uri + "/sessions", body: { "email": email, "password": password })
+
+      @auth_token = response["auth_token"]
+      raise "AuthorizationError" if @auth_token.empty?
+      
+    rescue
+      puts "Invalid email or password: Auth Token not returned."
+    end
   end
 end
