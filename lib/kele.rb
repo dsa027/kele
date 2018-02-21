f542c7fa9b586a2e8b06d9f77e1f0f56b5a19b45
David Alderson <dsa027@gmail.com>
2018-02-21 22:36:23 +0000

    clean up directories and instance vars

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,9 +1,11 @@
 require 'httparty'
 require './lib/roadmap'
+require 'awesome_print'
 
 class Kele
   include HTTParty
   include Roadmap
+  include AwesomePrint
 
   def initialize(email, password)
     begin
@@ -20,36 +22,13 @@ class Kele
   
   def get_me
     response = self.class.get(@base_uri + "/users/me", headers: {"authorization": @auth_token}).body
-    @me = JSON.parse(response)
+
+    return JSON.parse(response)
   end
   
   def get_mentor_availability(id)#2363254
     response = self.class.get(@base_uri + "/mentors/#{id}/student_availability", headers: {"authorization": @auth_token}).body
-    @mentor_avail = JSON.parse(response).to_a
-  end
-  
-  def puts_v(array) 
-    array.each do |v|
-      if v.is_a?(Hash)
-        puts ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> v is a HASH"
-        puts_kv(v)
-      else
-        puts "@@@@@@@@@@@@@@@@@@@ #{v}"
-      end
-    end
-  end
-  
-  def puts_kv(hash)
-    hash.each do |k, v| 
-      if v.is_a?(Hash)
-        puts ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> #{k} = "
-        puts_kv(v)
-      elsif v.is_a?(Array)
-        puts "@@@@@@@@@>>>>>>@@@@@@@@@@@@>>>>>>>>>>>>@@@@@@@@@@ #{k}"
-        puts_v(v)
-      else
-        puts ">>>>>>>>>>>>>>>>>>> #{k} = #{v}"
-      end
-    end
+
+    return JSON.parse(response).to_a
   end
 end
--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -1,13 +1,13 @@
 module Roadmap
   def get_roadmap(id)#37
     response = self.class.get(@base_uri + "/roadmaps/#{id}", headers: {"authorization": @auth_token}).body
-    @roadmap = JSON.parse(response)
+
+    return JSON.parse(response)
   end
   
   def get_checkpoint(id)#2123
     response = self.class.get(@base_uri + "/checkpoints/#{id}", headers: {"authorization": @auth_token}).body
-    @checkpoint = JSON.parse(response)
-    
-    puts_kv(@checkpoint)
+
+    return JSON.parse(response)
   end
 end
